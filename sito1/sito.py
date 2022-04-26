0cdf76fb42037130ae13485f07e804af88f9da69
Rubenxx00 <ruben.scopacasa@gmail.com>
2022-04-26 12:38:51 +0200

    add dispensa

--- sito1/sito.py
+++ sito1/sito.py
@@ -10,14 +10,19 @@ def home():
     return "Pagina iniziale (root del sito)"
 
 @app.route("/pagina1")
-def hello():
+def pagina1():
     return "<p>Hello World!</p>"
 
 @app.route("/pagina2")
-def hello2():
-    contenuto =  "<p>Ciao sono la pagina 2</p>" # racchiuso in un tag p
+def pagina2():
+    # costruisce il contenuto della pagina, creando una stringa
+    contenuto = "<p>Ciao sono la pagina 2</p>" # racchiuso in un tag p
     contenuto = contenuto + '<p>Calcolo la radice di 121: ' + str(sqrt(121)) + '</p>' # aggiungere un paragrafo che riporti la radice quadrata di 121
     return contenuto
+
+# definire pagina 3 che riporti:
+#       titolo "i primi 10 numeri pari"
+#       paragrafo contenente i primi 10 numeri pari (calcolati al momento)
 
 # serve a eseguire direttamente il file corrente, lancia la webapp creata e definita prima
 if __name__ == "__main__":
