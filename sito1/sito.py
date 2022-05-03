b188142b8cc2c0dab50c3b5fc79c6a4ee400c14a
Rubenxx00 <ruben.scopacasa@gmail.com>
2022-05-03 12:13:38 +0200

    add table

--- sito1/sito.py
+++ sito1/sito.py
@@ -7,7 +7,7 @@ app = Flask(__name__) # creazione app
 
 @app.route("/")
 def home():
-    return "Pagina iniziale (root del sito)"
+    return "Pagina iniziale (root del sito)  "
 
 @app.route("/pagina1")
 def pagina1():
@@ -23,7 +23,35 @@ def pagina2():
 # definire pagina 3 che riporti:
 #       titolo "i primi 10 numeri pari"
 #       paragrafo contenente i primi 10 numeri pari (calcolati al momento)
+@app.route("/pagina3")
+def pagina3():
+    contenuto = "<ul style='list-style-type: square;'>"
+    for i in range(1,21):
+        if i % 2 == 0:
+            contenuto += "<li>" + str(i) + "</li>"
+    contenuto += "</ul>"
+    return contenuto
+
+@app.route("/pagina4")
+def pagina4():
+    nomi = ['Chris', 'Dennis', 'Sarah']
+    età = [18, 22, 25]
+    contenuto = '''<table border="2">
+                        <tr>
+                            <th>Person</th>
+                            <th>Age</th>
+                        </tr>'''
+
+    # genero le righe
+    for i in range(len(nomi)):
+        contenuto += "<tr>"
+        contenuto += "<td>" + nomi[i] + "</td>" 
+        contenuto += "<td>" + str(età[i]) + "</td>"
+        contenuto += "</tr>"
+
+    contenuto += "</table>" # chiude tabella
+    return contenuto
 
 # serve a eseguire direttamente il file corrente, lancia la webapp creata e definita prima
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
